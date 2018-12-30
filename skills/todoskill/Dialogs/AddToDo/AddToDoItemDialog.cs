cb3030cd1002d01d44d67a55a4a2095ab8ce7c78
Johnsonzhou <zhouyq@outlook.com>
2018-12-30 11:38:56 +0800

    添加项目文件。

---
+++ skills/todoskill/Dialogs/AddToDo/AddToDoItemDialog.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Solutions.Skills;
+
+namespace ToDoSkill
+{
+    public class AddToDoItemDialog : ToDoSkillDialog
+    {
+        public AddToDoItemDialog(
+            ISkillConfiguration services,
+            IStatePropertyAccessor<ToDoSkillState> toDoStateAccessor,
+            IStatePropertyAccessor<ToDoSkillUserState> userStateAccessor,
+            ITaskService serviceManager,
+            IMailService mailService,
+            IBotTelemetryClient telemetryClient)
+            : base(nameof(AddToDoItemDialog), services, toDoStateAccessor, userStateAccessor, serviceManager, mailService, telemetryClient)
+        {
+            TelemetryClient = telemetryClient;
+
+            var addToDoTask = new WaterfallStep[]
+            {
+                GetAuthToken,
+                AfterGetAuthToken,
+                ClearContext,
+                CollectToDoTaskContent,
+                AddToDoTask,
+            };
+
+            var collectToDoTaskContent = new WaterfallStep[]
+            {
+                AskToDoTaskContent,
+                AfterAskToDoTaskContent,
+            };
+
+            // Define the conversation flow using a waterfall model.
+            AddDialog(new WaterfallDialog(Action.AddToDoTask, addToDoTask) { TelemetryClient = telemetryClient });
+            AddDialog(new WaterfallDialog(Action.CollectToDoTaskContent, collectToDoTaskContent) { TelemetryClient = telemetryClient });
+
+            // Set starting dialog for component
+            InitialDialogId = Action.AddToDoTask;
+        }
+    }
+}
